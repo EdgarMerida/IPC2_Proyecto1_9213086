d81a7c8bfad0a8de07c42669f50969b6fe5887f0
EdgarMerida <1672836500101@ingenieria.usac.edu.gt>
2021-03-06 13:00:22 -0600

    Primer commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import menu as menu
+from lista_circular import lista_circular
+
+
+"""
+l = lista_circular()
+l.agregar_fin(10)
+l.agregar_fin(56)
+l.agregar_fin(78)
+l.agregar_fin(90)
+
+l.recorrer()
+"""
+menu.display()
+
+
